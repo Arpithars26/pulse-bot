fb9d8bc9f1a6606a31fe1f0a7c30fb190dd27892
Arpitha R S <arpithars2006@gmail.com>
2026-06-13 14:32:10 +0530

    Added news secret

--- news_bot.py
+++ news_bot.py
@@ -1,20 +1,36 @@
 import requests
 import os
 
-API_KEY = os.environ["OPENWEATHER_API_KEY"]
-CITY = "Kochi"
+API_KEY = os.environ["NEWS_API_KEY"]
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+url = f"https://newsapi.org/v2/everything?q=technology&apiKey={API_KEY}"
 
-response = requests.get(url)
-data = response.json()
+data = requests.get(url).json()
 
-temp = data["main"]["temp"]
+html = """
+<html>
+<body>
+<h1>Today's Top Headlines</h1>
+<hr>
+"""
 
-print("City:", CITY)
-print("Temperature:", temp)
+for article in data["articles"][:5]:
 
-if temp > 35:
-    print("ALERT: Temperature above 35°C")
-else:
-    print("Temperature Normal")
+    title = article["title"]
+    link = article["url"]
+
+    html += f"""
+    <h3>{title}</h3>
+    <a href="{link}">Read Full Article</a>
+    <hr>
+    """
+
+html += """
+</body>
+</html>
+"""
+
+with open("news_email.html", "w", encoding="utf-8") as file:
+    file.write(html)
+
+print("HTML file created")
