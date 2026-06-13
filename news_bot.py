6c192929895f4a570be860ef452cd477a56eab6f
Arpitha R S <arpithars2006@gmail.com>
2026-06-13 14:29:31 +0530

    Added weather secret

--- news_bot.py
+++ news_bot.py
@@ -1,37 +1,20 @@
 import requests
+import os
 
-API_KEY = "9c1d2e90df6540609399721e49c2289b"
+API_KEY = os.environ["OPENWEATHER_API_KEY"]
+CITY = "Kochi"
 
-url = f"https://newsapi.org/v2/everything?q=technology&apiKey={API_KEY}"
+url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
-data = requests.get(url).json()
+response = requests.get(url)
+data = response.json()
 
-html = """
-<html>
-<body>
-<h1>Today's Top Headlines</h1>
-<hr>
-"""
+temp = data["main"]["temp"]
 
-for article in data["articles"][:5]:
+print("City:", CITY)
+print("Temperature:", temp)
 
-    title = article["title"]
-    link = article["url"]
-    date = article["publishedAt"]
-
-    html += f"""
-    <h3>{title}</h3>
-    <p>Published: {date}</p>
-    <a href="{link}">Read Full Article</a>
-    <hr>
-    """
-
-html += """
-</body>
-</html>
-"""
-
-with open("news_email.html", "w", encoding="utf-8") as file:
-    file.write(html)
-
-print("HTML file created successfully")
+if temp > 35:
+    print("ALERT: Temperature above 35°C")
+else:
+    print("Temperature Normal")
--- weather_alert.py
+++ weather_alert.py
@@ -1,13 +1,13 @@
 import requests
+import os
 
-API_KEY = "52cf7ce7e48d73989979337d1007abc4"
+API_KEY = os.environ["OPENWEATHER_API_KEY"]
+
 CITY = "Kochi"
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
 
-response = requests.get(url)
-
-data = response.json()
+data = requests.get(url).json()
 
 temp = data["main"]["temp"]
 
@@ -15,6 +15,6 @@ print("City:", CITY)
 print("Temperature:", temp)
 
 if temp > 35:
-    print("ALERT: Temperature above 35°C")
+    print("WEATHER ALERT")
 else:
-    print("Temperature Normal")
+    print("NO ALERT")
